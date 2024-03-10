a1e54b77dba326dc80781522847545522e4804dd
Vasylyna08 <141525962+Vasylyna08@users.noreply.github.com>
2024-03-11 00:51:40 +0200

    Add components

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Profile from './components/Profile/Profile';
+import userData from './components/Profile/userData.json';
+
+import FriendList from './components/FriendList/FriendList';
+import friends from './components/FriendList/friends.json';
+
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './components/TransactionHistory/transactions.json';
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        key={userData.id}
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,39 @@
+const TransactionHistory = ({ items }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(item => {
+          return (
+            <tr key={item.id}>
+              <Transaction
+                type={item.type}
+                amount={item.amount}
+                currency={item.currency}
+              />
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+const Transaction = ({ type, amount, currency }) => {
+  return (
+    <>
+      <td>{type}</td>
+      <td>{amount}</td>
+      <td>{currency}</td>
+    </>
+  );
+};
+
+export default TransactionHistory;
